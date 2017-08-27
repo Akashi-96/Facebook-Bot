1b64a70339ea37e68de7334f2444820f4d336dd4
Akash Agarwal <akashag.aa.1996@gmail.com>
2017-08-27 15:56:51 +0530

    minor changes

--- FacebookAPI.py
+++ FacebookAPI.py
@@ -1,7 +1,7 @@
 import requests, json
 from flask import url_for
 
-def get_user_fb(token, user_id):
+def getFBUserid(token, user_id):
     r = requests.get("https://graph.facebook.com/v2.6/" + user_id,
                     params={"fields": "first_name,last_name,profile_pic,locale,timezone,gender"
                         ,"access_token": token
@@ -9,10 +9,10 @@ def get_user_fb(token, user_id):
     if r.status_code != requests.codes.ok:
         print r.text
         return
-    user = json.loads(r.content)
+    user = json.loads(r.content) # Convert Python dictionary to JSON object.
     return user
 
-def show_typing(token, user_id, action='typing_on'):
+def showTyping(token, user_id, action='typing_on'):
   r = requests.post("https://graph.facebook.com/v2.6/me/messages",
                       params={"access_token": token},
                       data=json.dumps({
@@ -23,20 +23,22 @@ def show_typing(token, user_id, action='typing_on'):
   if r.status_code != requests.codes.ok:
         print r.text
 
-def send_message(token, user_id, text):
+def sendBackMsg(token, user_id, text):
     """Send the message text to recipient with id recipient.
     """
     r = requests.post("https://graph.facebook.com/v2.6/me/messages",
                       params={"access_token": token},
-                      data=json.dumps({
+                      data=json.dumps({ # Convert JSON object to Python Dictionary data structure.
                           "recipient": {"id": user_id},
                           "message": {"text": text.decode('unicode_escape')}
                       }),
-                      headers={'Content-type': 'application/json'})
+                      headers={'Content-type': 'application/json'}) # This is the MIME Type: This tells
+                                                                    # the browser that the python script 
+                                                                    # returns the data in the form of JSON Object.
     if r.status_code != requests.codes.ok:
         print r.text
 
-def send_picture(token, user_id, imageUrl, title="", subtitle=""):
+def sendBackPicture(token, user_id, imageUrl, title="", subtitle=""):
     if title != "":
         data = {"recipient": {"id": user_id},
                   "message":{
@@ -71,7 +73,8 @@ def send_picture(token, user_id, imageUrl, title="", subtitle=""):
     if r.status_code != requests.codes.ok:
         print r.text    
 
-def send_quick_replies_yelp_search(token, user_id):
+def sendQuickReplies(token, user_id):
+    # An example of structure os options and Object.
     # options = [Object {name:value, url:value}, Object {name:value, url:value}]
     quickRepliesOptions = [
         {"content_type":"text",
@@ -90,7 +93,7 @@ def send_quick_replies_yelp_search(token, user_id):
                 "quick_replies": quickRepliesOptions
                 }
             })
-    data = data.encode('utf-8')
+    data = data.encode('utf-8') # encode the data in UTF-8 format for further enhanced support.
     r = requests.post("https://graph.facebook.com/v2.6/me/messages",
         params={"access_token": token},
         data=data,
@@ -99,7 +102,7 @@ def send_quick_replies_yelp_search(token, user_id):
     if r.status_code != requests.codes.ok:
         print r.text
 
-def send_quick_replies_yelp_save_location(token, user_id, location=None):
+def sendQuickRepliesSaveLocation(token, user_id, location=None):
     # options = [Object {name:value, url:value}, Object {name:value, url:value}]
     quickRepliesOptions = [
         {"content_type":"text",
@@ -138,7 +141,7 @@ def send_quick_replies_yelp_save_location(token, user_id, location=None):
     if r.status_code != requests.codes.ok:
         print r.text
 
-def send_quick_replies_yelp_suggest_location(token, user_id, locations):
+def sendQuickRepliesSuggestLocation(token, user_id, locations):
     quickRepliesOptions = []
     i = 0
     for location in locations:
@@ -167,7 +170,7 @@ def send_quick_replies_yelp_suggest_location(token, user_id, locations):
     if r.status_code != requests.codes.ok:
         print r.text
 
-def send_yelp_results(token, user_id, businesses):
+def sendBackResults(token, user_id, businesses):
     options = []
 
     for business in businesses:
@@ -299,7 +302,7 @@ def send_intro_screenshots(app, token, user_id):
     if r.status_code != requests.codes.ok:
         print r.text
 
-def send_trending_news(token, user_id, posts):
+def sendBackTrendingNews(token, user_id, posts):
     options = []
     for post in posts:
         img_url = post['image_url'] if post['image_url'] != "" else url_for('static', filename='assets/img/empty-placeholder.jpg', _external=True)
@@ -376,6 +379,3 @@ def set_get_started_button():
     print r.content
     if r.status_code != requests.codes.ok:
         print r.text
-
-# set_menu()
-# set_get_started_button()
--- facebook-echobot-standalone.py
+++ facebook-echobot-standalone.py
@@ -1,29 +1,41 @@
 """
-Simple Facebook Echo bot: Respond with exactly what it receives
-Standalone version
+Created by: Akash Agarwal
+Institute of Engineering and Technology,Lucknow
+
+This is a simple implementation of a Facebook Chatbot:
+The language used is Python.
+Resources used- we require several python libraries: 
+Flask: This library is used to create a local host on the device.
+Since the Facebook requires a SSL Webhook for verification of a safe site, we require certain modularities-
+The facebook provides the Developer Account with "Access Token" (PAT), also makes use of Heroku and ngrok
+for setting up secure server.
 """
 
 import sys, json, traceback, requests
 from flask import Flask, request
 
-application = Flask(__name__)
-app = application
+app = Flask(__name__)
 PAT = 'EAABnOxb3Ck8BAH57uXZALlswSCKMuvmUZCTtZASfDMLmImyXrHgn2tBpQpIntR1eKeIczlgNZCitiShUOMNzGMoRpxyUGbW4CdWMJ1bFkjXm7us2PWZAwZBSxmwcQ5NDIAfUWdJKUKdkkZCj3DNWBFfr4v9DOS3MQLDPgqnPel6iQZDZD'
-VERIFICATION_TOKEN = 'Akash.1996'
+VERIFICATION_TOKEN = 'Akash.1996' # This is the verification code that we have to enter in the facebook account.
 
 @app.route('/', methods=['GET'])
-def handle_verification():
+def webhookVerification():
+    '''
+        This function handles the verification process for the webhooks required to attach bot to the 
+        facebook page.
+    '''
     print "Handling Verification."
-    if request.args.get('hub.verify_token', '') == VERIFICATION_TOKEN:
+    if request.args.get('hub.verify_token', '') == VERIFICATION_TOKEN:  # If the verfication code entered at
+                                                                        # at the FB Developers page matches to 
+                                                                        # the above defined value.
         print "Webhook verified!"
         return request.args.get('hub.challenge', '')
     else:
         return "Wrong verification token!"
 
-# ======================= Bot processing ===========================
-@app.route('/', methods=['POST'])
-def handle_messages():
-    payload = request.get_data()
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Functions defined that handle BOT Processing~~~~~~~~~~~~~~~~~~~~~~ #
+def msgHandler():
+    payload = request.get_data()    # request.get_data() requests for the data entered in the chat box
 
     # Handle messages
     for sender_id, message in messaging_events(payload):
@@ -31,32 +43,42 @@ def handle_messages():
         try:
             response = processIncoming(sender_id, message)
             
-            if response is not None:
-                send_message(PAT, sender_id, response)
+            if response is not None:    # if response if not NUll, i.e. some message is received.
+                send_message(PAT, sender_id, response) # then reply back the same message to the user.
 
             else:
-                send_message(PAT, sender_id, "Sorry I don't understand that")
+                send_message(PAT, sender_id, "Sorry, I couldn't understand what you just said.")
         except Exception, e:
             print e
             traceback.print_exc()
     return "ok"
 
 def processIncoming(user_id, message):
+    ''' 
+        This functions keep track of 3 types of input that the user can enter:
+        1) Text
+        2) Location
+        3) Audio
+        In case of text message, resend it.
+        In case of location, write a simple message while sending the link to the location.
+        In case of music, write a simple message while sending a link to the same audio.
+    '''
+
     if message['type'] == 'text':
         message_text = message['data']
         return message_text
 
     elif message['type'] == 'location':
-        response = "I've received location (%s,%s) (y)"%(message['data'][0],message['data'][1])
+        response = "Received a location (%s,%s) (y)"%(message['data'][0],message['data'][1])
         return response
 
     elif message['type'] == 'audio':
         audio_url = message['data']
-        return "I've received audio %s"%(audio_url)
+        return "Received an audio %s"%(audio_url)
 
-    # Unrecognizable incoming, remove context and reset all data to start afresh
+    # If the data received is unrecognisable, remove the context and reset all data to start as new.
     else:
-        return "*scratch my head*"
+        return "*unrecognised data received!!*"
 
 
 def send_message(token, user_id, text):
@@ -76,17 +98,17 @@ def send_message(token, user_id, text):
 # This part technically clean up received data to pass only meaningful data to processIncoming() function
 def messaging_events(payload):
     
-    data = json.loads(payload)
+    data = json.loads(payload) # json.loads() converts python dictionary to json object
     messaging_events = data["entry"][0]["messaging"]
     
     for event in messaging_events:
         sender_id = event["sender"]["id"]
 
-        # Not a message
+        # If it is not a message, i.e. nothing is received
         if "message" not in event:
             yield sender_id, None
 
-        # Pure text message
+        # If the message is a simple text message
         if "message" in event and "text" in event["message"] and "quick_reply" not in event["message"]:
             data = event["message"]["text"].encode('unicode_escape')
             yield sender_id, {'type':'text', 'data': data, 'message_id': event['message']['mid']}
@@ -94,7 +116,7 @@ def messaging_events(payload):
         # Message with attachment (location, audio, photo, file, etc)
         elif "attachments" in event["message"]:
 
-            # Location 
+            # If the attachment is a location 
             if "location" == event['message']['attachments'][0]["type"]:
                 coordinates = event['message']['attachments'][
                     0]['payload']['coordinates']
@@ -103,7 +125,7 @@ def messaging_events(payload):
 
                 yield sender_id, {'type':'location','data':[latitude, longitude],'message_id': event['message']['mid']}
 
-            # Audio
+            # If the attachment is an audio file
             elif "audio" == event['message']['attachments'][0]["type"]:
                 audio_url = event['message'][
                     'attachments'][0]['payload']['url']
@@ -120,9 +142,11 @@ def messaging_events(payload):
         else:
             yield sender_id, {'type':'text','data':"I don't understand this", 'message_id': event['message']['mid']}
 
-# Allows running with simple `python <filename> <port>`
-if __name__ == '__main__':
-    if len(sys.argv) == 2: # Allow running on customized ports
-        app.run(port=int(sys.argv[1]))
+
+if __name__ == '__main__':  # If the file is the first file, treat as main() 
+    if len(sys.argv) == 2: 
+        app.run(port=int(sys.argv[1]))  
+        # sys.argv is a list in Python, it contains the command-line arguments passed to the script
+        # len(sys.argv) counts the number of such arguments.
     else:
-        app.run() # Default port 5000
+        app.run() # Run Default port
